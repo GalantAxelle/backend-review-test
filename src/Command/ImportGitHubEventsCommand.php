90a4188a058562e1e2307f6a5881350cc9ab9f61
Florent Brassart <florent@ausha.co>
2024-03-31 19:28:43 +0200

    WIP: Command

--- src/Command/ImportGitHubEventsCommand.php
+++ src/Command/ImportGitHubEventsCommand.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use App\Client\GhArchiveClient;
+use phpDocumentor\Reflection\Types\False_;
+use SebastianBergmann\Environment\Console;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\DumpCompletionCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 /**
  * This command must import GitHub events.
@@ -16,17 +22,79 @@ class ImportGitHubEventsCommand extends Command
 {
     protected static $defaultName = 'app:import-github-events';
 
+    private $ghArchiveClient;
+
+    public function __construct(GhArchiveClient $ghArchiveClient, string $name = null)
+    {
+        parent::__construct($name);
+        $this->ghArchiveClient = $ghArchiveClient;
+    }
+
     protected function configure(): void
     {
+        //TODO: arguments ou options ?
         $this
-            ->setDescription('Import GH events');
+            ->setDescription('Import GH events related to a specific keyword')
+            ->addOption('date', null, InputOption::VALUE_REQUIRED, 'The date of the events you want to fetch. It should respect this format: YYYY-MM-DD')
+            ->addOption('hour', null, InputOption::VALUE_REQUIRED, 'The hour of the events you want to fetch. It should respect this format: H or HH');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // Let's rock !
-        // It's up to you now
+        $io = new SymfonyStyle($input, $output);
+        $date = $input->getOption('date');
+        $hour = $input->getOption('hour');
 
-        return 1;
+        if (empty($date) || empty($hour) || !$this->isDateFormatCorrect($date) || !$this->isHourFormatCorrect($hour)) {
+            $io->error('Both date and hour options must be provided with the correct format.');
+            return Command::FAILURE;
+        }
+
+        $io->info("Fetching events for date $date and hour $hour");
+        try {
+            //TODO: Call the GhArchiveEventRepository
+            $io->success('Successfully fetched events from GHArchive');
+        } catch (\Exception $e) {
+            $io->error('Could not retrieve events from GHArchive.');
+        }
+
+
+
+
+        return Command::SUCCESS;
+    }
+
+    private function isDateFormatCorrect(string $date): bool
+    {
+        try {
+            $dateTime = new \DateTime($date);
+            $formattedDate = $dateTime->format('Y-m-d');
+
+            if ($formattedDate !== $date) {
+                return false;
+            }
+
+            $startDate = new \DateTime('2015-01-01');
+            $endDate = new \DateTime('now');
+
+            if ($dateTime < $startDate || $dateTime > $endDate) {
+                return false;
+            }
+        } catch (\Exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function isHourFormatCorrect(string $hour): bool
+    {
+        $hour = (int)$hour;
+
+        if ($hour >= 0 && $hour <= 23) {
+            return true;
+        }
+
+        return false;
     }
 }
