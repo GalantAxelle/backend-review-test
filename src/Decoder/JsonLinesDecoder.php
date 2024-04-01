34e3e9a659f6ee6fbfa608e31def1b7086dd278f
Florent Brassart <florent@ausha.co>
2024-04-01 12:38:18 +0200

    fix: Ignore empty JSON lines

--- src/Decoder/JsonLinesDecoder.php
+++ src/Decoder/JsonLinesDecoder.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace App\Decoder;
 
+use JsonException;
+
 class JsonLinesDecoder
 {
+    /**
+     * @throws JsonException
+     */
     public function decodeJsonLinesToArray(string $serializedContent): array
     {
         $serializedLines = explode(PHP_EOL, $serializedContent);
@@ -13,6 +18,10 @@ class JsonLinesDecoder
         $decodedLines = [];
 
         foreach ($serializedLines as $line) {
+            if (true === empty($line)) {
+                continue;
+            }
+
             $decodedLines[] = json_decode($line, true, 512, JSON_THROW_ON_ERROR);
         }
 
